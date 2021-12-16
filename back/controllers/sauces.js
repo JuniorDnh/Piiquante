02376850803b6e160de9787044fd17b45d6a2c44
SonimJunior <sonim7@hotmail.fr>
2021-12-16 19:35:15 +0100

    commit

--- back/controllers/sauces.js
+++ back/controllers/sauces.js
@@ -1,5 +1,39 @@
 const Sauces = require('../models/sauces');
 
-exports.createThing = (req, res, next) => {
+// Créer une sauce
+exports.createSauce = (req, res, next) => {
+    const sauceObject = JSON.parse(req.body.sauce);
+  delete sauceObject._id;
+  const sauce = new Sauce ({ 
+    ...sauceObject, 
+    imageUrl : `${req.protocol}://${req.get('host')}/images/${req.file.filename}` 
+  });
+  sauce.save() 
+    .then(() => res.status(201).json ({message : "Sauce créée!"})) 
+    .catch(error => res.status(400).json({error}));
+};
 
-}
+// Modifier une sauce
+exports.modifySauce = (req, res, next) => {
+
+};
+
+//Supprimer une sauce
+exports.deleteSauce = (req, res, next) => {
+
+};
+
+// Selectionner une sauce
+exports.getOneSauce = (req, res, next) => {
+
+};
+
+// Selectionner toutes les sauces
+exports.getAllSauces = (req, res, next) => {
+
+};
+
+// Like ou Dislike une sauce
+exports.LikeDislikeSauce = (req, res, next) => {
+
+};
