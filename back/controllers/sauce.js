c911a4b1a72333337981f53a20a04033630d1768
SonimJunior <sonim7@hotmail.fr>
2021-12-23 17:47:57 +0100

    commit

--- back/controllers/sauce.js
+++ back/controllers/sauce.js
@@ -1,21 +1,18 @@
 const Sauce = require("../models/sauce");
 const fs = require("fs");
-
+// Créer une sauce
 exports.createSauce = (req, res, next) => {
   const sauceObject = JSON.parse(req.body.sauce);
-  delete sauceObject._id;
-  const sauce = new Sauce({
-    ...sauceObject,
-    imageUrl: `${req.protocol}://${req.get("host")}/images/${
-      req.file.filename
-    }`,
-  });
-  sauce
-    .save()
-    .then(() => res.status(201).json({ message: "Sauce créée!" }))
-    .catch((error) => res.status(400).json({ error }));
+delete sauceObject._id;
+const sauce = new Sauce ({ 
+  ...sauceObject, 
+  imageUrl : `${req.protocol}://${req.get('host')}/images/${req.file.filename}` 
+});
+sauce.save() 
+  .then(() => res.status(201).json ({message : "Sauce créée!"})) 
+  .catch(error => res.status(400).json({error}));
 };
-
+// Modifier une sauce
 exports.modifySauce = (req, res, next) => {
   if (req.file) {
     Sauce.findOne({ _id: req.params.id })
@@ -47,7 +44,7 @@ exports.modifySauce = (req, res, next) => {
       .catch((error) => res.status(400).json({ error }));
   }
 };
-
+// Supprimer une sauce
 exports.deleteSauce = (req, res, next) => {
   Sauce.findOne({ _id: req.params.id })
     .then((sauce) => {
@@ -60,19 +57,19 @@ exports.deleteSauce = (req, res, next) => {
     })
     .catch((error) => res.status(500).json({ error }));
 };
-
+// Afficher une sauce
 exports.getOneSauce = (req, res, next) => {
   Sauce.findOne({ _id: req.params.id })
     .then((sauce) => res.status(200).json(sauce))
     .catch((error) => res.status(404).json({ error }));
 };
-
+// Afficher toutes les sauces
 exports.getAllSauces = (req, res, next) => {
   Sauce.find()
     .then((sauces) => res.status(200).json(sauces))
     .catch((error) => res.status(400).json({ error }));
 };
-
+// Like ou Dislike une sauce
 exports.likeDislikeSauce = (req, res, next) => {
   Sauce.findOne({ _id: req.params.id })
     .then((sauce) => {
